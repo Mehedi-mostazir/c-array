82f17020b232c3e8267b492ffe3bc63e358f873f
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-01-20 23:40:56 +0530

    even element of an array

---
+++ evenelementarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int size;
+	scanf("%d",&size);
+	int a[size],i;
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0; i<size; i++)
+	{
+	
+	if( a[i]%2 == 0)
+	{
+		printf("%d\n",a[i];
+	}
+	}
+}
