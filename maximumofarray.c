7fe7aad6d104bdf3e6a1845e963a6c14f66ef9bf
Mehedi Mostazir <97426129+Mehedi-mostazir@users.noreply.github.com>
2022-01-22 00:18:56 +0530

    maximum number in the given array

---
+++ maximumofarray.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int size;
+	scanf("%d",&size);
+	int arr[size],i,r,m,t;
+	
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i=0;i<size;i++)
+	{
+		m=arr[i];
+		r=arr[i-1];
+		if(m > r)
+		{
+			t=arr[i];
+		}
+	
+	}
+	printf("%d",t); 
+}
